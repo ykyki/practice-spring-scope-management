5a8c1354d88e3707898df1e7dcbb8487c2965fb8
ykyki <bsyki@pm.me>
2024-02-06 18:38:25 +0900

    spring project is initialized

---
+++ src/main/java/com/example/practice/spring/scope/management/PracticeSpringScopeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice.spring.scope.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringScopeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringScopeManagementApplication.class, args);
+	}
+
+}
